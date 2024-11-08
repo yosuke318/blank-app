a300941022ffbfc6d2de45b056d5087f4b3f993c
KisukeUrakara <73513978+yosuke318@users.noreply.github.com>
2024-11-08 04:00:52 +0000

    authoricate

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,41 @@
 import streamlit as st
 
-st.title("🎈 My new streamlit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+if "role" not in st.session_state:
+    st.session_state.role = None
+
+ROLES = [None, "Requester", "Responder", "Admin"]
+
+
+def login():
+    st.header("Log in")
+
+    role = st.selectbox("Choose your role", ROLES)
+
+    if st.button("Log in"):
+        st.session_state.role = role
+        st.rerun()
+
+    
+def logout():
+    st.session_state.role = None
+    st.rerun()
+
+
+role = st.session_state.role
+
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
+
+request_1 = st.Page(
+    "request/request_1.py",
+    title="Request 1",
+    icon=":material/help:",
+    default=(role=="Requester"),
 )
+
+request_2 = st.Page(
+    "request/request_2.py", title="Request 2", icon=":material/bug_report:"
+)
+
+
